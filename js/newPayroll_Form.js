263569a293bf1b8c78b281cabdc64e3ba888b23e
k12e <kalyanirathod2095@gmail.com>
2022-06-06 14:26:36 +0530

    Added: new attributes and getter setter method

---
+++ js/newPayroll_Form.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+      if (name.value.length == 0) {
+        nameError.textContent = "";
+        return;
+      }
+      try {
+        (new EmployeePayrollData()).name = name.value;
+        nameError.textContent = "";
+      }
+      catch (e) {
+        nameError.textContent = e;
+      }
+    });
+});
