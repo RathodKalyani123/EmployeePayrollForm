1a60a9b5a53cbe0486b6da403ea50d187dd71f86
k12e <kalyanirathod2095@gmail.com>
2022-06-07 12:31:40 +0530

    Added: on save create payroll object

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -1,149 +1,85 @@
+//UC10
+class EmployeePayrollData {
 
-    class EmployeePayrollData{
-        get id() {
-            return this._id;
-        }
-    
-        set id(id) {
-            this._id = id;
-        }
-      get name(){
-            return this._name;
-        }
-        set name(name){
-            let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
-            if(nameRegex.test(name))
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
             this._name = name;
-            else
-            throw "NAME IS INCORRECT!!!!!!!!!";
-        }
-    
-        get profileImage(){
-            return this._profileImage;
-        }
-        set profileImage(profileImage){
-            this._profileImage = profileImage;
-        }
-        get gender(){
-            return this._gender;
-        }
-        set gender(gender){
-            let genderRegex = RegExp('^[female|male]+$');
-            if(genderRegex.test(gener))
-            this._gender = gender;
-            else
-            throw "Gendder is incorrect" ;
-        }
-        get department(){
-            return this._department;
-        }
-        set department(department){
-            this._department =department;
-        }
-        get salary(){
-            return this._salary;
-        }
-        set salary(salary){
-        let salaryRegx = RegExp('^[1-9][0-9]*$');
-        if(salaryRegx.test(salary))
-        this._salary =salary;
         else
-        throw "Salary is incorrect !!!"
-        }
-        get startDate(){
-            return this._startDate;
-        }
-        set startDate(startDate){
-            this._startDate = startDate;
-        }
-    
-        get notes() {
-            return this._notes;
-        }
-    
-        set notes(notes) {
-            this._notes = notes;
-        }
-    
-        toString() {
-            const options = { year: 'numeric', month: 'long', day: 'numeric' };
-            const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
-            return "Name = " + this.name + ", Profile Image = " + this.profileImage 
-            + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = "
-             + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+            throw "Name is Incorrect";
+    }
+
+    get profileImage() {
+        return this._profileImage;
+    }
+
+    set profileImage(profileImage) {
+        this._profileImage = profileImage;
+    }
+
+    get gender() {
+        return this._gender;
+    }
+
+    set gender(gender) {
+        this._gender = gender;
+    }
+
+    get department() {
+        return this._department;
+    }
+
+    set department(department) {
+        this._department = department;
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        this._salary = salary;
+
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        let difference = Date.now() - startDate;
+        difference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+        if (difference > 30 || difference < 0) {
+          throw "Start Date is Invalid";
+        } else {
+          this._startDate = startDate;
         }
     }
 
+    get notes() {
+        return this._notes;
+    }
 
-    window.addEventListener('DOMContentLoaded', (event) => {
-        const name = document.querySelector('#name');
-        const nameError = document.querySelector('.text-error');
-        name.addEventListener('input', function () {
-          if (name.value.length == 0) {
-            nameError.textContent = "";
-            return;
-          }
-          try {
-            (new EmployeePayrollData()).name = name.value;
-            nameError.textContent = "";
-          }
-          catch (e) {
-            nameError.textContent = e;
-          }
-        });
-    
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salary.addEventListener('input', function() {
-        output.textContent = salary.value;
-        });
-    });
-    
-        const save = () => {
-            try{
-                let employeePayrollData=createEmployeePayroll();
-                createAndUpdateStorage(employeePayrollData);
-            }catch(e){
-                return;
-            }
-        }
+    set notes(notes) {
+        this._notes = notes;
+    }
 
-        const createEmployeePayroll=()=>{
-            let employeePayrollData=new EmployeePayrollData();
-            try{
-                employeePayrollData._name=getInputValueById('#name');
-            }catch (e) {
-                setTextValue('.text-error',e);
-                throw e;
-             }
-             employeePayrollData.profileImage=getSelectedValues('[name=profile]').pop();
-             employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
-             employeePayrollData.department=getSelectedValues('[name=department]').pop();
-             employeePayrollData.salary=getInputValueById('#salary');
-             employeePayrollData.notes=getInputValueById('#notes');
-             let Date=getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
-             employeePayrollData.Date=Date.parse(Date);
-             alert(employeePayrollData.toString());
-             return employeePayrollData;
 
-        }
-
-        const getSelectedValues=(propertyvalue) => {
-            let allItems=document.querySelectorAll(propertyvalue);
-            let setItems=[];
-            allItems.forEach(item => {
-           if(item.checked) setItems.push(item.value);
-            });
-            return setItems;
-        }
-
-        const getInputValueById = (id) =>{
-            let value=document.querySelector(id).value;
-            return value;
-        }
-
-        const getInputElementValue= (id) =>{
-            let value=document.getElementById(id).value;
-            return value;
-        }
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ", Name = " + this.name + ", Profile Image = " + this.profileImage + ", Gender = " + this.gender + ", Department = " + this.department + ", Salary = " + this.salary + ", Start Date = " + employeeDate + ", Notes = " + this.notes;
+    }
+}
