ba876efb22ea103d01c17fd3f4cd02edad4526bb
k12e <kalyanirathod2095@gmail.com>
2022-06-08 12:49:32 +0530

    Added: employee details from JSON object

--- js/new_home.js
+++ js/new_home.js
@@ -5,31 +5,97 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const createInnerHtml = () => {
 
 const headerHtml =
-    `<tr>
-        <th>Profile</th>
+       `<th>Profile</th>
         <th>Name</th>
         <th>Gender</th>
         <th>Department</th>
         <th>Salary</th>
         <th>Start Date</th>
-        <th>Actions</th>
-    </tr>`;
+        <th>Actions</th>`;
 
-const innerHtml = `${headerHtml}
-    <tr>
-    <td><img class = "Profile" src="../assets/images/Ellipse -01.png" alt="profile_img-1"></td>
-    <td>Surraiya Nirban</td>
-    <td>female</td>
-    <td>
-        <div class="dept-label">HR</div>
-        <div class="dept-label">Engineer</div>
-    </td>
-    <td>&#8377; 5000000</td>
-    <td>07 jun 2022</td>
-    <td>
+let innerHtml = `${headerHtml}`;
+let employeePayrollDataList = createEmployeePayrollJSON();
+
+for(const employeePayrollData of employeePayrollDataList){
+innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" src="${employeePayrollData._profileImage}" alt="profile_img-1"></td>
+        <td>${employeePayrollData._name}</td>
+        <td>${employeePayrollData._gender}</td>
+        <td>${getDepartmentHtml(employeePayrollData._department)}</td>
+        
+        <td>${employeePayrollData._salary}</td>
+        <td>${employeePayrollData._startDate}</td>
+        <td>
         <img src="../assets/images/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
         <img src="../assets/images/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
-    </td>
+        </td>
     </tr>`;
+}
 document.querySelector('#table-display').innerHTML = innerHtml;
 }
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+      {
+        _name: 'Kalyani Rathod',
+        _gender: 'female',
+        _department: [
+          'HR',
+          'Engineer'
+        ],
+        _salary: 20000000,
+        _startDate: '07 jun 2022',
+        _note: 'Employee payroll application',
+        _id: new Date().getTime(),
+        _profileImage: '../assets/images/Ellipse -01.png'
+      },
+      {
+        _name: 'Ayansh Jadhao',
+        _gender: 'male',
+        _department: [
+          'HR',
+          'sales'
+        ],
+        _salary: 4000000,
+        _startDate: '07 jun 2022',
+        _note: 'Employee details',
+        _id: new Date().getTime(),
+        _profileImage: '../assets/images/Ellipse -03.png'
+      },
+      {
+        _name: 'Raj kawale',
+        _gender: 'male',
+        _department: [
+          'HR'
+         
+        ],
+        _salary: 50000000,
+        _startDate: '08 jun 2022',
+        _note: 'Employee payroll application',
+        _id: new Date().getTime(),
+        _profileImage: '../assets/images/Ellipse -08.png'
+      },
+      {
+        _name: 'Swati Badole',
+        _gender: 'female',
+        _department: [
+          'HR',
+          'sales'
+        ],
+        _salary: 60000000,
+        _startDate: '07 jun 2022',
+        _note: 'Employee payroll application',
+        _id: new Date().getTime(),
+        _profileImage: '../assets/images/Ellipse -07.png'
+      },
+    ];
+    return employeePayrollListLocal;
+  }
+  const getDepartmentHtml = (departmentList) => {
+    let departmentHtml = '';
+    for(const department of departmentList){
+      departmentHtml = `${departmentHtml} <div class='dept-label'>${department}</div>`
+    }
+    return departmentHtml
+  }
