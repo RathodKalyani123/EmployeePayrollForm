d139b0d25f343b80b16c647ce7c401acf56291e8
k12e <kalyanirathod2095@gmail.com>
2022-06-09 15:15:19 +0530

    Added: use remove function

--- js/new_home.js
+++ js/new_home.js
@@ -1,5 +1,4 @@
-  /* uc 6*/
-  let employeePayrollList;
+let employeePayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
     employeePayrollList = getEmployeePayrollDataFromStorage();
     document.querySelector('.emp-count').textContent = employeePayrollList.length;
@@ -21,15 +20,15 @@ let innerHtml = `${headerHtml}`;
 for(const employeePayrollData of employeePayrollList){
 innerHtml = `${innerHtml}
     <tr>
-        <td><img class="profile" src="${employeePayrollData._profileImage}"></td>
+        <td><img class="profile" src="${employeePayrollData._profileImage}"></td>  
         <td>${employeePayrollData._name}</td>
         <td>${employeePayrollData._gender}</td>
         <td>${getDepartmentHtml(employeePayrollData._department)}</td>
         <td>${employeePayrollData._salary}</td>
-        <td>${stringifyDate(employeePayrollData._startDate)}</td>
+        <td>${employeePayrollData.fullDate}</td>
         <td>
-        <img name =${employeePayrollData._id} src="../assets/images/delete-black-18dp.svg" alt="delete" id="1" onclick="remove(this)">
-        <img name =${employeePayrollData._id} src="../assets/images/create-black-18dp.svg" alt="edit" id="1" onclick="update(this)">
+        <img id =${employeePayrollData._id} src="../assets/images/delete-black-18dp.svg" alt="delete" onclick="remove(this.id)">
+        <img id =${employeePayrollData._id} src="../assets/images/create-black-18dp.svg" alt="edit" onclick="update(this.id)">
         </td>
     </tr>`;
 }
@@ -43,4 +42,24 @@ const getDepartmentHtml = (departmentList) => {
     }
     return departmentHtml
 }
-  
+
+//Day 46-UC1::: Remove function
+const remove = (node) => {
+  let employeePayrollData = employeePayrollList.find(empData => empData._id == node.id);
+  if (!employeePayrollData) return;
+  const index = employeePayrollList
+                .map(empData => empData._id)
+                .indexOf(employeePayrollData._id);
+  employeePayrollList.splice(index, 1);
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+  createInnerHtml();
+}
+
+/*day46_uc2*/
+
+const update = (node) => {
+    let employeePayrollData = employeePayrollList.find(empData => empData._name == node.id)
+    if (!employeePayrollData) return;
+    localStorage.setItem('editEmp', JSON.stringify(employeePayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page)
+  }
